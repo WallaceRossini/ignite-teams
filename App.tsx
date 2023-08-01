1cb10bb5ef96faf223c1abcf8994eb9054528147
Wallace Rossini <rossiniwallace@gmail.com>
2023-07-31 22:05:08 -0300

    feat: Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { Groups } from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import theme from './src/theme';
 
+import { Groups } from '@screens/Groups';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    REGULAR: Roboto_400Regular,
+    BOLD: Roboto_700Bold
+  })
+
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
