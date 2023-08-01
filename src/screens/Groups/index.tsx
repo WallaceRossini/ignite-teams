9d5650532f4b79f85a5664e7c954a7de623e3f87
Wallace Rossini <rossiniwallace@gmail.com>
2023-08-01 00:03:31 -0300

    feat: Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Container } from './styles';
 import { Header } from '@components/Header'
 import { GroupCard } from '@components/GroupCard';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -22,6 +23,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeria turma?'
           />
         )}
       />
