26be4750c2fd8f218b9d08dfd2ecb87f466bd454
Wallace Rossini <rossiniwallace@gmail.com>
2023-07-31 22:13:07 -0300

    feat: Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +16,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
