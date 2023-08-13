6b679dc419c9f8a29c3e1dbd58018d27cf4f1e73
Wallace Rossini <rossiniwallace@gmail.com>
2023-08-13 19:55:04 -0300

    refactor: Utilizando o CSS Helper

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,19 +1,19 @@
 import { TouchableOpacity } from 'react-native'
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 
-export type ButtonTypeStyleProps =  'PRIMARY' | 'SECONDARY'
+export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY'
 
 type Props = {
   type: ButtonTypeStyleProps
 }
 
-export const Container = styled(TouchableOpacity)<Props>`
+export const Container = styled(TouchableOpacity) <Props>`
   flex: 1;
   
   min-height: 56px;
   max-height: 56px;
 
-  background: ${({theme, type}) => type === 'PRIMARY' ? theme.COLORS.GREEN_700: theme.COLORS.RED_DARK };
+  background: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
 
   justify-content: center;
   align-items: center;
@@ -22,7 +22,9 @@ export const Container = styled(TouchableOpacity)<Props>`
 `
 
 export const Title = styled.Text`
-  font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-  color: ${({theme}) => theme.COLORS.WHITE};
-  font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};
+${({ theme }) => css`
+  font-size: ${theme.FONT_SIZE.MD}px;
+  color:  ${theme.COLORS.WHITE};
+  font-family: ${theme.FONT_FAMILY.BOLD};
+`}
 `
